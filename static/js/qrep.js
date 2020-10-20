d69d6928e0098e0b2a96b6a23b8f987935523804
dan <me@danrh.co.uk>
2020-10-20 18:38:32 +0200

    Can toggle issue resolved/unresolved from webgui

--- static/js/qrep.js
+++ static/js/qrep.js
@@ -1,48 +1,52 @@
+var QrepController = {
+    getCheckBox: function (resolved) { return resolved ? "check_box" : "check_box_outline_blank";},
+    setIssues: function (issues) {
+        const printIssue = (issue) => {
+            return `
+            <li>
+               <div class="collection-item">
+                  <i class="material-icons">whatshot</i> 
+                  ${issue.description} 
+                  <a style="cursor:pointer;" class="secondary-content" onclick="QrepController.toggleIssueResolved('${issue.id}')">
+                     <i id="${issue.id}resolved" class="material-icons">${QrepController.getCheckBox(issue.resolved)}</i>
+                  </a> 
+               </div>
+            </li>
+          `;
+        }
+        var issuelist = "";
+        for (i of issues) {
+          issuelist += printIssue(i);
+        }
+        
+        document.getElementById("issues_view").innerHTML =
+        `<ul class="collection">
+              ${issuelist}
+         </ul>
+        `;
+    },
 
-function setIssues(issues) {
-    const printIssue = (issue) => {
-        return `
-        <li>
-           <div class="collection-item">
-              <i class="material-icons">whatshot</i> 
-              ${issue.description} 
-              <a href="#!" class="secondary-content" onclick="toggleIssueResolved('${issue.id}')">
-                 <i class="material-icons">checkbox</i>
-              </a> 
-           </div>
-        </li>
-      `;
+    displayIssues: function (id) {
+        issues = itemissues.find(item => item.id==id).issues
+        QrepController.setIssues(issues);
+    },
+
+    toggleShow: function (id) {
+      var x = document.getElementById(id);
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    },
+
+    toggleIssueResolved: function (x) {
+        console.log(x);
+        QrepService.toggleIssueResolved(x);
+    },
+
+    setResolved: function (id, resolved) {
+        document.getElementById(id+"resolved").innerHTML = QrepController.getCheckBox(resolved)
+        M.toast({html: resolved ? "Issue resolved!" : "Issue unresolved."});
     }
-    var issuelist = "";
-    for (i of issues) {
-      issuelist += printIssue(i);
-    }
-    
-    document.getElementById("issues_view").innerHTML =
-    `<ul class="collection">
-          ${issuelist}
-     </ul>
-    `;
-}
-
-
-function displayIssues(id) {
-    issues = itemissues.find(item => item.id==id).issues
-    setIssues(issues);
-}
-
-
-function toggleShow(id) {
-  var x = document.getElementById(id);
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-
-
-function toggleIssueResolved(x) {
-    console.log(x);
-    QrepService.putIssue(x);
 }
--- static/js/qrepservice.js
+++ static/js/qrepservice.js
@@ -14,12 +14,26 @@ var QrepService = {
       // do something with myJson
       console.log(myJson);
     },
+
+    toggleIssueResolved: async (issueId) => {
+      var i = QrepService.findIssue(itemissues, issueId);
+      i.resolved = !i.resolved;
+      const response = await fetch("/issue/" + issueId , {
+        method: 'PUT',
+        body: JSON.stringify(i), // string or object
+        headers: {
+          'Content-Type': 'application/json'
+        }
+     });
+      const myJson = await response.json();
+      console.log(myJson);
+      QrepController.setResolved(i.id, i.resolved)
+    },
+
     findIssue: (itemissues, issueId) => {
       let issueIdMatch = (issue) => issue.id==issueId;
       return itemissues
         .find(item => item.issues.some(issueIdMatch))
         .issues.find(issueIdMatch);
     }
-
-
 }
